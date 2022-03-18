35b8521bbf5a7b8bdf97f157c10a50f46e325766
emilio <emilio@mpbwalt.local>
2022-03-18 22:28:32 +0100

    fix

--- applications/__init__.py
+++ applications/__init__.py
@@ -1,5 +1,5 @@
 from .application1 import Application1
 from .applicationMVA import ApplicationMVA
-from .applicationsCluster import AppsCluster
+#from .applicationsCluster import AppsCluster
 
 from .application import Application
